6613376a7a18379759cce5a647ff476fae2b2aef
T3p <matteo.papini@polimi.it>
2018-03-16 10:36:29 +0100

    cartpole experiment

--- script/plot_progress.py
+++ script/plot_progress.py
@@ -1,18 +1,29 @@
 import matplotlib.pyplot as plt
 import csv
+import numpy as np
+import scipy.stats as sts
+
+delta = .05
 
 avg_return = []
 bound = []
-with open('progress.csv') as fp:
+var = []
+with open('../results/trpo/cheetah/progress.csv') as fp:
     reader = csv.DictReader(fp)
     for row in  reader:
         avg_return.append(float(row['EpRewMean']))
-        bound.append(float(row['StudentTBound']))
+        J_hat = float(row['J_hat'])
+        var_J = float(row['Var_J'])
+        batch_size = int(float(row['EpLenMean']))
+        b = J_hat - sts.t.ppf(1-delta, batch_size -1) * \
+                     np.sqrt(var_J/batch_size)
+        bound.append(min(b, 4000))
 
+"""
 for x in range(len(avg_return)):
     plt.axvline(x, color='y')
+"""
 plt.plot(range(len(avg_return)),avg_return)
 plt.plot(range(len(bound)), bound)
-
 plt.show()
 
