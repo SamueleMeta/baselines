8ff2bd8afc5ce007b7a18c329a567add3be495f4
T3p <matteo.papini@polimi.it>
2018-05-01 00:59:03 +0200

    lqg experiment

--- script/run_poispe.py
+++ script/run_poispe.py
@@ -11,7 +11,6 @@ import baselines.envs.continuous_cartpole
 import baselines.envs.lqg1d
 from baselines.policy.pemlp_policy import PeMlpPolicy
 import baselines.pgpe.poisnpe as poisnpe
-import baselines.pgpe.poispe as poispe
 import numpy as np
 
 import baselines.common.tf_util as U
@@ -22,17 +21,9 @@ envs = {'cartpole': 'ContCartPole-v0',
         'lqg': 'LQG1D-v0',
         }
 
-algos = {'poisnpe': poisnpe,
-         'poispe': poispe,
-        }
-
 horizons = {'cartpole': 200,
             'lqg': 200,
             }
-
-rews = {'cartpole': 10,
-      'lqg': 28.8,
-      }
 
 iters = {'cartpole': 100,
          'lqg': 100,
@@ -40,21 +31,15 @@ iters = {'cartpole': 100,
 
 #Seeds: 107, 583, 850, 730, 808
 
-gamma = 1.
-
-def train(seed, env_name, algo_name, normalize, use_rmax, use_renyi):
-    DIR = 'temp/'
-    index = int(str(int(normalize)) + str(int(use_rmax)) + str(int(use_renyi)), 2)
-    #DIR = '../results/' + algo_name + '/noshift_fix/bound_' + str(index) + '/' + env_name + '/seed_' + str(seed)
+def train(seed, env_name, shift, normalize, use_rmax, use_renyi, path):
+    index = int(str(int(shift)) + str(int(normalize)) + str(int(use_rmax)) + str(int(use_renyi)), 2)
+    DIR = path + '/poisnpe/bound_' + str(index) + '/' + env_name + '/seed_' + str(seed)
     import os
     if not os.path.exists(DIR):
         os.makedirs(DIR)
     
     env = gym.make(envs[env_name])
     env.seed(seed)
-    horizon = horizons[env_name]
-    #rmax = sum([rews[env_name]*gamma**i for i in range(horizon)])
-    rmax = None #Empirical
     
     pol_maker = lambda name: PeMlpPolicy(name,
                       env.observation_space,
@@ -64,12 +49,12 @@ def train(seed, env_name, algo_name, normalize, use_rmax, use_renyi):
                       use_bias=False,
                       seed=seed)
     
-    algos[algo_name].learn(env,
+    poisnpe.learn(env,
               pol_maker,
-              gamma=gamma,
+              gamma=1.,
               batch_size=100,
-              task_horizon=horizon,
-              max_iterations=100,
+              task_horizon=horizons[env_name],
+              max_iterations=iters[env_name],
               save_to=DIR,
               verbose=2,
               feature_fun=np.ravel,
@@ -78,21 +63,23 @@ def train(seed, env_name, algo_name, normalize, use_rmax, use_renyi):
               use_renyi=use_renyi,
               max_offline_ite=100,
               max_search_ite=30,
-              rmax=rmax,
               delta=0.2,
-              shift=True,
-              reuse=True)
+              shift=shift)
 
 if __name__=='__main__':
     import argparse
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--path', help='save here', type=str, default='temp')
     parser.add_argument('--seed', help='RNG seed', type=int, default=None)
+    parser.add_argument('--shift', help='Normalize return?', type=int, default=0)
     parser.add_argument('--normalize', help='Normalize weights?', type=int, default=1)
     parser.add_argument('--use_rmax', help='Use Rmax in bound (or var)?', type=int, default=1)
     parser.add_argument('--use_renyi', help='Use Renyi in ESS (or weight norm)?', type=int, default=1)
-    parser.add_argument('--algo', help='Algorithm', type=str, default='poisnpe')
-    parser.add_argument('--env', help='Environment (RL task)', type=str, default='cartpole')
+    parser.add_argument('--env', help='Environment (RL task)', type=str, default='lqg')
     args = parser.parse_args()
-    train(args.seed, args.env, args.algo, args.normalize, 
+    train(args.seed, args.env, 
+          args.shift,
+          args.normalize, 
           args.use_rmax,
-          args.use_renyi)
+          args.use_renyi,
+          args.path)
