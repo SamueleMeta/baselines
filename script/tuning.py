90fc203d65c94434dcd488225d2ba32b7748a54a
matteo <matteo.papini@mail.polimi.it>
2018-05-11 23:20:15 +0200

    results

--- script/tuning.py
+++ script/tuning.py
@@ -10,9 +10,10 @@ from screener import Screener
 deltas = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.]
 deltas = map(str, deltas)
 path = 'tuning_network'
+seed = 109
 
-commands = ['python3 sequential_experiment.py --path %s --delta %s' % (path + '/delta_' + delta.replace('.',''),
-                                                                                    delta)
+commands = ['python3 run_multi_poispe_rllab_cartpole.py --path %s --delta %s --seed %d' % (path + '/delta_' + delta.replace('.',''),
+                                                                                    delta, seed)
                 for delta in deltas]
 
 Screener().run(commands, name='tuning_network')
