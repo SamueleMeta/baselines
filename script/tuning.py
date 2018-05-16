2d3744f79169cd75e83dbfc3d2d68eac6bcfd550
matteo <matteo.papini@polimi.it>
2018-05-16 09:44:51 +0200

    r

--- script/tuning.py
+++ script/tuning.py
@@ -11,13 +11,13 @@ import random
 deltas = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.]
 deltas = map(str, deltas)
 path = 'tuning'
-env = 'ant'
+env = 'swimmer'
 script = 'run_poispe_rllab_mujoco_par'
 
 commands = ['python3 sequential_experiment.py --path %s --delta %s --env %s --script %s' % (path + '/delta_' + delta.replace('.',''),
                                                                                     delta, env, script)
                 for i, delta in enumerate(deltas)]
 
-Screener().run(commands, name='tuning_ant%d' % int(random.random()*1e6))
+Screener().run(commands, name='tuning_swimmer%d' % int(random.random()*1e6))
 for c in commands:
     print(c)
