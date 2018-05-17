4f6ad06ea7005171e9520f4c50ac73acb3b8f770
matteo <matteo.papini@polimi.it>
2018-05-17 09:40:40 +0200

    network tuning results

--- script/run_multi_poispe.py
+++ script/run_multi_poispe.py
@@ -16,19 +16,45 @@ import numpy as np
 
 from baselines.envs.rllab_wrappers import Rllab2GymWrapper
 from rllab.envs.box2d.cartpole_env import CartpoleEnv
+from rllab.envs.box2d.cartpole_swingup_env import CartpoleSwingupEnv 
+from rllab.envs.box2d.mountain_car_env import MountainCarEnv 
+from rllab.envs.box2d.double_pendulum_env import DoublePendulumEnv
+from rllab.envs.mujoco.inverted_double_pendulum_env import InvertedDoublePendulumEnv
+from rllab.envs.mujoco.swimmer_env import SwimmerEnv
+from rllab.envs.mujoco.hopper_env import HopperEnv
+from rllab.envs.mujoco.walker2d_env import Walker2DEnv
+from rllab.envs.mujoco.half_cheetah_env import HalfCheetahEnv
+from rllab.envs.mujoco.ant_env import AntEnv
+from rllab.envs.mujoco.simple_humanoid_env import SimpleHumanoidEnv
+from rllab.envs.mujoco.humanoid_env import HumanoidEnv
 
 import baselines.common.tf_util as U
 from baselines.common import set_global_seeds
 
-def train(seed, shift, normalize, use_rmax, use_renyi, path, delta):
+ 
+envs = {'cartpole': CartpoleEnv, 
+        'inverted_pendulum': CartpoleSwingupEnv, 
+        'mountain_car': MountainCarEnv, 
+        'acrobot': DoublePendulumEnv}
+envs['double_inverted_pendulum'] = InvertedDoublePendulumEnv
+envs['swimmer'] = SwimmerEnv
+envs['hopper'] = HopperEnv
+envs['walker2d'] = Walker2DEnv
+envs['half_cheetah'] = HalfCheetahEnv
+envs['ant'] = AntEnv
+envs['simple_humanoid'] = SimpleHumanoidEnv
+envs['humanoid'] = HumanoidEnv
+
+
+def train(seed, shift, normalize, use_rmax, use_renyi, path, delta, env_name):
     index = int(str(int(shift)) + str(int(normalize)) + str(int(use_rmax)) + str(int(use_renyi)), 2)
-    DIR = '../results/' + path + '/cartpole/' + str.replace(delta, '.', '_')  + '/seed_' + str(seed)
+    DIR = '../results/' + path + '/' + env_name  + '/delta_' + str.replace(delta, '.', '')  + '/seed_' + str(seed)
     import os
     if not os.path.exists(DIR):
         os.makedirs(DIR)
     
     def env_maker():
-        env = CartpoleEnv()
+        env = envs[env_name]()
         env = Rllab2GymWrapper(env)
         return env
         
@@ -81,6 +107,7 @@ if __name__=='__main__':
     parser.add_argument('--use_rmax', help='Use Rmax in bound (or var)?', type=int, default=1)
     parser.add_argument('--use_renyi', help='Use Renyi in ESS (or weight norm)?', type=int, default=0)
     parser.add_argument('--delta', help='delta', type=str, default='0.8')
+    parser.add_argument('--env', help='task name', type=str, default='mountain_car')
     args = parser.parse_args()
     train(args.seed,
           args.shift,
@@ -88,4 +115,5 @@ if __name__=='__main__':
           args.use_rmax,
           args.use_renyi,
           args.path,
-          args.delta)
+          args.delta,
+          args.env)
