eefbd485b102292bdac233b4fea2c11c16630f5c
nicomon24 <nico.montali24@gmail.com>
2018-07-23 18:32:26 +0200

    Added regexp to screen killer

--- script/screen_killer.py
+++ script/screen_killer.py
@@ -1,5 +1,12 @@
 from screenutils import list_screens
+import argparse, re
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--re', help='RegExp to select dying screens.', default='*')
+args = parser.parse_args()
+
+rule = re.compile(args.re)
 
 for s in list_screens():
-    if s.name != 'tensorboard':
+    if rule.match(s):
         s.kill()
