03d956eef4d50dc72175c89193e07309c2c2831f
nicomon24 <nico.montali24@gmail.com>
2018-08-29 17:31:35 +0200

    Fixed entbonus to ent_f

--- baselines/pois/pois.py
+++ baselines/pois/pois.py
@@ -533,7 +533,7 @@ def learn(make_env, make_policy, *,
     # Add policy entropy bonus
     if entcoeff != 0:
         ent_f = tf.minimum(0.0, 1-tf.abs(tf.reduce_mean(iw) - 1)) * entcoeff
-        losses_with_name.append((entbonus, 'EntropyCoefficient'))
+        losses_with_name.append((ent_f, 'EntropyCoefficient'))
         bound_ = bound_ + ent_f * meanent
 
     if use_natural_gradient:
