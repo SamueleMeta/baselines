4cc720fb7896a66ba86086d2aae4efb1c11f034a
nicomon24 <nico.montali24@gmail.com>
2018-08-30 11:19:18 +0200

    Used tf.less_equal instead of <

--- baselines/pois/pois.py
+++ baselines/pois/pois.py
@@ -530,7 +530,7 @@ def learn(make_env, make_policy, *,
     if entropy != 'none':
         scheme, v1, v2 = entropy.split(':')
         if scheme == 'step':
-            entcoeff = tf.cond(iter_progress_ < int(v2), lambda: float(v1), lambda: 0.0)
+            entcoeff = tf.cond(tf.less_equal(iter_progress_, float(v2)), lambda: float(v1), lambda: 0.0)
             losses_with_name.append((entcoeff, 'EntropyCoefficient'))
             entbonus = entcoeff * meanent
             bound_ = bound_ + entbonus
