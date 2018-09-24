a3d22ad51f93ad3723846b5984d4dd91a613b539
nicomon24 <nico.montali24@gmail.com>
2018-09-24 18:59:08 +0200

    Try without cloudpickle

--- baselines/common/vec_env/subproc_vec_env.py
+++ baselines/common/vec_env/subproc_vec_env.py
@@ -5,7 +5,7 @@ from baselines.common.vec_env import VecEnv, CloudpickleWrapper
 
 def worker(remote, parent_remote, env_fn_wrapper):
     parent_remote.close()
-    env = env_fn_wrapper.x()
+    env = env_fn_wrapper()
     while True:
         cmd, data = remote.recv()
         if cmd == 'step':
@@ -37,7 +37,7 @@ class SubprocVecEnv(VecEnv):
         self.closed = False
         nenvs = len(env_fns)
         self.remotes, self.work_remotes = zip(*[Pipe() for _ in range(nenvs)])
-        self.ps = [Process(target=worker, args=(work_remote, remote, CloudpickleWrapper(env_fn)))
+        self.ps = [Process(target=worker, args=(work_remote, remote, env_fn))
             for (work_remote, remote, env_fn) in zip(self.work_remotes, self.remotes, env_fns)]
         for p in self.ps:
             p.daemon = True # if the main process crashes, we should not cause things to hang
@@ -74,7 +74,7 @@ class SubprocVecEnv(VecEnv):
         if self.closed:
             return
         if self.waiting:
-            for remote in self.remotes:            
+            for remote in self.remotes:
                 remote.recv()
         for remote in self.remotes:
             remote.send(('close', None))
