db9bca4bc91a005335de600a7ab418e19e09b563
Matteo <matteo.papini@polimi.it>
2018-10-11 15:27:32 +0200

    pbpois refactoring: to test

---
+++ baselines/pbpois/run_gym.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+from baselines import bench, logger
+import time, os, gym, logging
+import tensorflow as tf
+import numpy as np
+
+import baselines.common.tf_util as U
+from baselines.common import set_global_seeds
+from baselines.policy.neuron_hyperpolicy import MultiPeMlpPolicy
+from baselines.pbpois import pbpois
+from baselines.pbpois.parallel_sampler import ParallelSampler
+
+def train(env, max_iters, num_episodes, horizon, bound, delta, gamma, seed, policy, max_offline_iters, njobs=1):
+    
+    # Create the environment
+    def make_env():
+        env_gym = gym.make(env)
+        return env_gym
+
+    # Create the policy
+    if policy == 'linear':
+        hid_layers = []
+    elif policy == 'nn':
+        hid_layers = [100, 50, 25]
+        
+    make_policy = lambda name, observation_space, action_space: MultiPeMlpPolicy(name,
+                      observation_space,
+                      action_space,
+                      hid_layers,
+                      use_bias=True,
+                      seed=seed)
+
+    sampler = ParallelSampler(make_env, make_policy, gamma, horizon, np.ravel, num_episodes, njobs, seed)
+
+    try:
+        affinity = len(os.sched_getaffinity(0))
+    except:
+        affinity = njobs
+    sess = U.make_session(affinity)
+    sess.__enter__()
+
+    set_global_seeds(seed)
+
+    gym.logger.setLevel(logging.WARN)
+    
+    pbpois.learn(
+          make_env, 
+          make_policy,
+          sampler,
+          gamma=gamma,
+          n_episodes=num_episodes,
+          horizon=horizon,
+          max_iters=max_iters,
+          save_to='.',
+          verbose=2,
+          feature_fun=np.ravel,
+          bound = bound,
+          max_offline_iters=max_offline_iters,
+          delta=delta,
+          center_return=False,
+          line_search_type='parabola')
+
+    sampler.close()
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--seed', help='RNG seed', type=int, default=0)
+    parser.add_argument('--env', type=str, default='cartpole')
+    parser.add_argument('--num_episodes', type=int, default=100)
+    parser.add_argument('--horizon', type=int, default=500)
+    parser.add_argument('--file_name', type=str, default='progress')
+    parser.add_argument('--bound', type=str, default='max-ess')
+    parser.add_argument('--delta', type=float, default=0.99)
+    parser.add_argument('--njobs', type=int, default=-1)
+    parser.add_argument('--policy', type=str, default='nn')
+    parser.add_argument('--max_offline_iters', type=int, default=20)
+    parser.add_argument('--max_iters', type=int, default=500)
+    parser.add_argument('--gamma', type=float, default=1.0)
+    args = parser.parse_args()
+    if args.file_name == 'progress':
+        file_name = '%s_delta=%s_seed=%s_%s' % (args.env.upper(), args.delta, args.seed, time.time())
+    else:
+        file_name = args.file_name
+    logger.configure(dir='logs', format_strs=['stdout', 'csv', 'tensorboard'], file_name=file_name)
+    train(env=args.env,
+          max_iters=args.max_iters,
+          num_episodes=args.num_episodes,
+          horizon=args.horizon,
+          iw_norm=args.iw_norm,
+          bound=args.bound,
+          delta=args.delta,
+          gamma=args.gamma,
+          seed=args.seed,
+          policy=args.policy,
+          max_offline_iters=args.max_offline_iters,
+          njobs=args.njobs)
+
+if __name__ == '__main__':
+    main()
+    
+    
