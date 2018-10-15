b53ab0e97f16c40eae1b220659e52d84e43e1976
nicomon24 <nico.montali24@gmail.com>
2018-10-15 18:09:32 +0200

    Merged atari, gym and rllab

---
+++ baselines/pois/run.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# noinspection PyUnresolvedReferences
+'''
+    This script runs rllab or gym environments. To run RLLAB, use the format
+    rllab.<env_name> as env name, otherwise gym will be used.
+'''
+# Common imports
+import sys, re, os, time, logging
+
+# Framework imports
+import gym
+import tensorflow as tf
+
+# Self imports: utils
+from baselines.common import set_global_seeds
+from baselines import logger
+import baselines.common.tf_util as U
+from baselines.common.rllab_utils import Rllab2GymWrapper, rllab_env_from_name
+# Self imports: algorithm
+from baselines.policy.mlp_policy import MlpPolicy
+from baselines.pois import pois
+from baselines.pois.parallel_sampler import ParallelSampler
+
+def get_env_type(env_id):
+    #First load all envs
+    _game_envs = defaultdict(set)
+    for env in gym.envs.registry.all():
+        # TODO: solve this with regexes
+        env_type = env._entry_point.split(':')[0].split('.')[-1]
+        _game_envs[env_type].add(env.id)
+    # Get env type
+    env_type = None
+    for g, e in _game_envs.items():
+        if env_id in e:
+            env_type = g
+            break
+    return env_type
+
+def train(env, max_iters, num_episodes, horizon, iw_method, iw_norm, natural, bound, delta, gamma, seed, policy, max_offline_iters, njobs=1):
+
+    if env.startswith('rllab.'):
+        # Get env name and class
+        env_name = re.match('rllab.(\w+)', env).group(1)
+        env_rllab_class = rllab_env_from_name(env_name)
+        # Define env maker
+        def make_env():
+            env_rllab = env_rllab_class()
+            _env = Rllab2GymWrapper(env_rllab)
+            return _env
+        # Used later
+        env_type = 'rllab'
+    else:
+        # Normal gym, get if Atari or not.
+        env_type = get_env_type(env)
+        assert env_type is not None, "Env not recognized."
+        # Define the correct env maker
+        if env_type == 'atari':
+            # Atari, custom env creation
+            def make_env():
+                _env = make_atari(env)
+                return wrap_deepmind(_env)
+        else:
+            # Not atari, standard env creation
+            def make_env():
+                env_rllab = gym.make(env)
+                return env_rllab
+
+    if policy == 'linear':
+        hid_size = num_hid_layers = 0
+    elif policy == 'nn':
+        hid_size = [100, 50, 25]
+        num_hid_layers = 3
+
+    if policy == 'linear' or policy == 'nn':
+        def make_policy(name, ob_space, ac_space):
+            return MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
+                             hid_size=hid_size, num_hid_layers=num_hid_layers, gaussian_fixed_var=True, use_bias=False, use_critic=False,
+                             hidden_W_init=tf.contrib.layers.xavier_initializer(),
+                             output_W_init=tf.contrib.layers.xavier_initializer())
+    elif policy == 'cnn':
+        def make_policy(name, ob_space, ac_space):
+            return CnnPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
+                         gaussian_fixed_var=True, use_bias=False, use_critic=False,
+                         hidden_W_init=tf.contrib.layers.xavier_initializer(),
+                         output_W_init=tf.contrib.layers.xavier_initializer())
+    else:
+        raise Exception('Unrecognized policy type.')
+
+    sampler = ParallelSampler(make_policy, make_env, num_episodes, horizon, True, n_workers=njobs, seed=seed)
+
+    try:
+        affinity = len(os.sched_getaffinity(0))
+    except:
+        affinity = njobs
+    sess = U.make_session(affinity)
+    sess.__enter__()
+
+    set_global_seeds(seed)
+
+    gym.logger.setLevel(logging.WARN)
+
+    pois.learn(make_env, make_policy, n_episodes=num_episodes, max_iters=max_iters,
+               horizon=horizon, gamma=gamma, delta=delta, use_natural_gradient=natural,
+               iw_method=iw_method, iw_norm=iw_norm, bound=bound, save_weights=True, sampler=sampler,
+               center_return=True, render_after=None, max_offline_iters=max_offline_iters,)
+
+    sampler.close()
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--seed', help='RNG seed', type=int, default=0)
+    parser.add_argument('--env', type=str, default='cartpole')
+    parser.add_argument('--num_episodes', type=int, default=100)
+    parser.add_argument('--horizon', type=int, default=500)
+    parser.add_argument('--iw_method', type=str, default='is')
+    parser.add_argument('--iw_norm', type=str, default='none')
+    parser.add_argument('--natural', type=bool, default=False)
+    parser.add_argument('--file_name', type=str, default='progress')
+    parser.add_argument('--bound', type=str, default='max-d2')
+    parser.add_argument('--delta', type=float, default=0.99)
+    parser.add_argument('--njobs', type=int, default=-1)
+    parser.add_argument('--policy', type=str, default='nn')
+    parser.add_argument('--max_offline_iters', type=int, default=10)
+    parser.add_argument('--max_iters', type=int, default=500)
+    parser.add_argument('--gamma', type=float, default=1.0)
+    args = parser.parse_args()
+    if args.file_name == 'progress':
+        file_name = '%s_delta=%s_seed=%s_%s' % (args.env.upper(), args.delta, args.seed, time.time())
+    else:
+        file_name = args.file_name
+    logger.configure(dir='logs', format_strs=['stdout', 'csv', 'tensorboard'], file_name=file_name)
+    train(env=args.env,
+          max_iters=args.max_iters,
+          num_episodes=args.num_episodes,
+          horizon=args.horizon,
+          iw_method=args.iw_method,
+          iw_norm=args.iw_norm,
+          natural=args.natural,
+          bound=args.bound,
+          delta=args.delta,
+          gamma=args.gamma,
+          seed=args.seed,
+          policy=args.policy,
+          max_offline_iters=args.max_offline_iters,
+          njobs=args.njobs)
+
+if __name__ == '__main__':
+    main()
