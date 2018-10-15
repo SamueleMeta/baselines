680d25e9b226920bd982b0db7f14a912a0e87019
nicomon24 <nico.montali24@gmail.com>
2018-10-15 19:28:07 +0200

    Merged also PBPOIS

--- baselines/pbpois/run.py
+++ baselines/pbpois/run.py
@@ -1,31 +1,78 @@
 #!/usr/bin/env python3
-from baselines import logger
-import time, os, gym, logging
+'''
+    This script runs rllab or gym environments. To run RLLAB, use the format
+    rllab.<env_name> as env name, otherwise gym will be used.
+'''
+# Common imports
+import sys, re, os, time, logging
+from collections import defaultdict
+
+# Framework imports
+import gym
+import tensorflow as tf
 import numpy as np
 
-import baselines.common.tf_util as U
+# Self imports: utils
 from baselines.common import set_global_seeds
+from baselines import logger
+import baselines.common.tf_util as U
+from baselines.common.rllab_utils import Rllab2GymWrapper, rllab_env_from_name
+from baselines.common.atari_wrappers import make_atari, wrap_deepmind
+# Self imports: algorithm
 from baselines.policy.neuron_hyperpolicy import MultiPeMlpPolicy
 from baselines.policy.weight_hyperpolicy import PeMlpPolicy
 from baselines.pbpois import pbpois, nbpois
 from baselines.pbpois.parallel_sampler import ParallelSampler
-from baselines.common.rllab_utils import Rllab2GymWrapper, rllab_env_from_name
+
+def get_env_type(env_id):
+    #First load all envs
+    _game_envs = defaultdict(set)
+    for env in gym.envs.registry.all():
+        # TODO: solve this with regexes
+        env_type = env._entry_point.split(':')[0].split('.')[-1]
+        _game_envs[env_type].add(env.id)
+    # Get env type
+    env_type = None
+    for g, e in _game_envs.items():
+        if env_id in e:
+            env_type = g
+            break
+    return env_type
 
 def train(env, max_iters, num_episodes, horizon, iw_norm, bound, delta, gamma, seed, policy, max_offline_iters, aggregate, adaptive_batch, njobs=1):
 
-    # Create the environment
-    env_rllab_class = rllab_env_from_name(env)
-    def make_env():
-        env_rllab = env_rllab_class()
-        _env = Rllab2GymWrapper(env_rllab)
-        return _env
+    if env.startswith('rllab.'):
+        # Get env name and class
+        env_name = re.match('rllab.(\w+)', env).group(1)
+        env_rllab_class = rllab_env_from_name(env_name)
+        # Define env maker
+        def make_env():
+            env_rllab = env_rllab_class()
+            _env = Rllab2GymWrapper(env_rllab)
+            return _env
+        # Used later
+        env_type = 'rllab'
+    else:
+        # Normal gym, get if Atari or not.
+        env_type = get_env_type(env)
+        assert env_type is not None, "Env not recognized."
+        # Define the correct env maker
+        if env_type == 'atari':
+            # Atari is not tested here
+            raise Exception('Not tested on atari.')
+        else:
+            # Not atari, standard env creation
+            def make_env():
+                env_rllab = gym.make(env)
+                return env_rllab
 
     # Create the policy
     if policy == 'linear':
         hid_layers = []
     elif policy == 'nn':
         hid_layers = [100, 50, 25]
-
+    elif policy == 'cnn':
+        raise Exception('CNN policy not tested.')
 
     if aggregate=='none':
         learner = pbpois
