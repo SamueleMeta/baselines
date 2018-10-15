81d21cc69d57f63bd43a11c784fec3297db55884
nicomon24 <nico.montali24@gmail.com>
2018-10-15 18:58:59 +0200

    Merged also POIS2

--- baselines/pois2/pois2.py
+++ baselines/pois2/pois2.py
@@ -17,55 +17,88 @@ def timed(msg):
     print(colorize('done in %.3f seconds'%(time.time() - tstart), color='magenta'))
 
 def traj_segment_generator(pi, env, n_episodes, horizon, stochastic, gamma):
-    # FIXME: we could discard the last parallel episodes
-    assert n_episodes % env.num_envs == 0, "Batch size must be multiple of the number of workers."
+
+    policy_time = 0
+    env_time = 0
 
     # Initialize state variables
     t = 0
     ac = np.array([env.action_space.sample()] * env.num_envs)
+
+    _env_s = time.time()
     ob = env.reset()
-    new = True
+    env_time += time.time() - _env_s
+    zero_ob = np.zeros(ob.shape)
 
-    cur_ep_ret = 0
-    cur_ep_len = 0
-    ep_rets = []
-    ep_lens = []
+    current_indexes = np.arange(0, env.num_envs)
 
-    # Initialize history arrays
-    obs = np.array([[ob[0] for _t in range(horizon)] for _e in range(n_episodes)])
-    rews = np.zeros((n_episodes, horizon), 'float32')
-    vpreds = np.zeros((n_episodes, horizon), 'float32')
-    #news = np.zeros(horizon * n_episodes, 'int32') #FIXME: what is my goal?
-    acs = np.array([[ac[0] for _t in range(horizon)] for _e in range(n_episodes)])
-    prevacs = acs.copy()
-    mask = np.ones((n_episodes, horizon), 'float32')
+    def filter_indexes(idx_vector, t_vector):
+        return map(list, zip(*[(i, v, t) for i,(v,t) in enumerate(zip(idx_vector, t_vector)) if v != -1]))
+
+    def has_ended(idx_vector):
+        return sum(idx_vector) == -len(idx_vector)
 
     # Iterate to make yield continuous
     while True:
-        for i in range(n_episodes // env.num_envs):
-            idx = i * env.num_envs
-            for j in range(horizon):
-                # Get the action and save the previous one
-                prevac = ac
-                ac, vpred = pi.act(stochastic, ob)
-                # Save the current properties
-                obs[idx:idx+env.num_envs,j,:] = ob
-                vpreds[idx:idx+env.num_envs,j] = vpred
-                acs[idx:idx+env.num_envs,j] = ac
-                prevacs[idx:idx+env.num_envs,j] = prevac
-                # Take the action
-                env.step_async(ac)
-                ob, rew, done, _ = env.step_wait()
-                # Save the reward
-                rews[idx:idx+env.num_envs, j] = rew
-                mask[idx:idx+env.num_envs, j] = np.invert(np.array(done))
-            # Reset the workers
-            ob = env.reset()
+
+        _tt = time.time()
+
+        # Initialize history arrays
+        obs = np.array([[zero_ob[0] for _t in range(horizon)] for _e in range(n_episodes)])
+        rews = np.zeros((n_episodes, horizon), 'float32')
+        vpreds = np.zeros((n_episodes, horizon), 'float32')
+        #news = np.zeros(horizon * n_episodes, 'int32') #FIXME: what is my goal?
+        acs = np.array([[ac[0] for _t in range(horizon)] for _e in range(n_episodes)])
+        prevacs = acs.copy()
+        mask = np.zeros((n_episodes, horizon), 'float32')
+        # Initialize indexes and timesteps
+        current_indexes = np.arange(0, env.num_envs)
+        current_timesteps = np.zeros((env.num_envs), dtype=np.int32)
+
+        while not has_ended(current_indexes):
+
+            # Get the action and save the previous one
+            prevac = ac
+
+            _pi_s = time.time()
+            ac, vpred = pi.act(stochastic, ob)
+            policy_time += time.time() - _pi_s
+
+            # Filter the current indexes
+            ci_ob, ci_memory, ct = filter_indexes(current_indexes, current_timesteps)
+
+            # Save the current properties
+            obs[ci_memory, ct,:] = ob[ci_ob]
+            #vpreds[ci_memory, ct] = np.reshape(np.array(vpred), (-1,))[ci_ob]
+            acs[ci_memory, ct] = ac[ci_ob]
+            prevacs[ci_memory, ct] = prevac[ci_ob]
+
+            # Take the action
+            _env_s = time.time()
+            env.step_async(ac)
+            ob, rew, done, _ = env.step_wait()
+            env_time += time.time() - _env_s
+
+            # Save the reward
+            rews[ci_memory, ct] = rew[ci_ob]
+            mask[ci_memory, ct] = np.invert(np.reshape(np.array(done), (-1,)))[ci_ob]
+
+            # Update the indexes and timesteps
+            for i, d in enumerate(done):
+                if not d and current_timesteps[i] < (horizon-1):
+                    current_timesteps[i] += 1
+                elif max(current_indexes) < n_episodes - 1:
+                    current_timesteps[i] = 0 # Reset the timestep
+                    current_indexes[i] = max(current_indexes) + 1 # Increment the index
+                else:
+                    current_indexes[i] = -1 # Disabling
 
         # Add discounted reward (here is simpler)
         gamma_log = np.log(np.full((horizon), gamma, dtype='float32'))
         gamma_discounter = np.exp(np.cumsum(gamma_log))
         discounted_reward = rews * gamma_discounter
+
+        total_time = time.time() - _tt
 
         # Reshape to flatten episodes and yield
         yield {'ob': np.reshape(obs, (n_episodes * horizon,)+obs.shape[2:]),
@@ -78,7 +111,14 @@ def traj_segment_generator(pi, env, n_episodes, horizon, stochastic, gamma):
                'ep_lens': np.sum(mask, axis=1),
                'mask': np.reshape(mask, (n_episodes * horizon)),
                'disc_rew': np.reshape(discounted_reward, (n_episodes * horizon)),
-               'ep_disc_ret': np.sum(discounted_reward, axis=1)}
+               'ep_disc_ret': np.sum(discounted_reward, axis=1),
+               'total_time': total_time,
+               'policy_time': policy_time,
+               'env_time': env_time}
+
+        # Reset time counters
+        policy_time = 0
+        env_time = 0
 
 def update_epsilon(delta_bound, epsilon_old, max_increase=2.):
     if delta_bound > (1. - 1. / (2 * max_increase)) * epsilon_old:
@@ -631,6 +671,12 @@ def learn(env, make_policy, *,
             logger.record_tabular("EpisodesSoFar", episodes_so_far)
             logger.record_tabular("TimestepsSoFar", timesteps_so_far)
             logger.record_tabular("TimeElapsed", time.time() - tstart)
+            # Time records
+            logger.record_tabular("TotalTime", seg['total_time'])
+            logger.record_tabular("PolicyTime", seg['policy_time'])
+            logger.record_tabular("EnvTime", seg['env_time'])
+            logger.record_tabular("PolicyRatio", seg['policy_time'] / seg['total_time'])
+            logger.record_tabular("EnvRatio", seg['env_time'] / seg['total_time'])
 
         if save_weights:
             logger.record_tabular('Weights', str(get_parameter()))
---
+++ baselines/pois2/run.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+'''
+    This script runs rllab or gym environments. To run RLLAB, use the format
+    rllab.<env_name> as env name, otherwise gym will be used.
+'''
+# Common imports
+import sys, re, os, time, logging
+from collections import defaultdict
+
+# Framework imports
+import gym
+import tensorflow as tf
+
+# Self imports: utils
+from baselines.common import set_global_seeds
+from baselines import logger
+import baselines.common.tf_util as U
+from baselines.common.rllab_utils import Rllab2GymWrapper, rllab_env_from_name
+from baselines.common.atari_wrappers import make_atari, wrap_deepmind
+from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
+from baselines.common.vec_env.vec_frame_stack import VecFrameStack
+# Self imports: algorithm
+from baselines.policy.mlp_policy import MlpPolicy
+from baselines.policy.cnn_policy import CnnPolicy
+from baselines.pois2 import pois2
+
+def get_env_type(env_id):
+    #First load all envs
+    _game_envs = defaultdict(set)
+    for env in gym.envs.registry.all():
+        # TODO: solve this with regexes
+        env_type = env._entry_point.split(':')[0].split('.')[-1]
+        _game_envs[env_type].add(env.id)
+    # Get env type
+    env_type = None
+    for g, e in _game_envs.items():
+        if env_id in e:
+            env_type = g
+            break
+    return env_type
+
+def train(env, max_iters, num_episodes, horizon, iw_method, iw_norm, natural, bound, delta, gamma, seed, policy, max_offline_iters, njobs=1):
+
+    if env.startswith('rllab.'):
+        # Get env name and class
+        env_name = re.match('rllab.(\w+)', env).group(1)
+        env_rllab_class = rllab_env_from_name(env_name)
+        # Define env maker
+        def make_env(seed=0):
+            def _thunk():
+                env_rllab = Rllab2GymWrapper(rllab_env_class())
+                env_rllab.seed(seed)
+                return env_rllab
+            return _thunk
+        parallel_env = SubprocVecEnv([make_env(i + seed) for i in range(njobs)])
+        # Used later
+        env_type = 'rllab'
+    else:
+        # Normal gym, get if Atari or not.
+        env_type = get_env_type(env)
+        assert env_type is not None, "Env not recognized."
+        # Define the correct env maker
+        if env_type == 'atari':
+            # Atari, custom env creation
+            def make_env(seed=0):
+                def _thunk():
+                    _env = make_atari(env)
+                    _env.seed(seed)
+                    return wrap_deepmind(_env)
+                return _thunk
+            parallel_env = VecFrameStack(SubprocVecEnv([make_env(i + seed) for i in range(njobs)]), 4)
+        else:
+            # Not atari, standard env creation
+            def make_env(seed=0):
+                def _thunk():
+                    _env = gym.make(env)
+                    _env.seed(seed)
+                    return _env
+                return _thunk
+            parallel_env = SubprocVecEnv([make_env(i + seed) for i in range(njobs)])
+
+    # Create the policy
+    if policy == 'linear':
+        hid_size = num_hid_layers = 0
+    elif policy == 'nn':
+        hid_size = [100, 50, 25]
+        num_hid_layers = 3
+
+    if policy == 'linear' or policy == 'nn':
+        def make_policy(name, ob_space, ac_space):
+            return MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
+                             hid_size=hid_size, num_hid_layers=num_hid_layers, gaussian_fixed_var=True, use_bias=False, use_critic=False,
+                             hidden_W_init=tf.contrib.layers.xavier_initializer(),
+                             output_W_init=tf.contrib.layers.xavier_initializer())
+    elif policy == 'cnn':
+        def make_policy(name, ob_space, ac_space):
+            return CnnPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
+                         gaussian_fixed_var=True, use_bias=False, use_critic=False,
+                         hidden_W_init=tf.contrib.layers.xavier_initializer(),
+                         output_W_init=tf.contrib.layers.xavier_initializer())
+    else:
+        raise Exception('Unrecognized policy type.')
+
+    try:
+        affinity = len(os.sched_getaffinity(0))
+    except:
+        affinity = njobs
+    sess = U.make_session(affinity)
+    sess.__enter__()
+
+    set_global_seeds(seed)
+
+    gym.logger.setLevel(logging.WARN)
+
+    pois2.learn(parallel_env, make_policy, n_episodes=num_episodes, max_iters=max_iters,
+               horizon=horizon, gamma=gamma, delta=delta, use_natural_gradient=natural,
+               iw_method=iw_method, iw_norm=iw_norm, bound=bound, save_weights=True,
+               center_return=True, render_after=None, max_offline_iters=max_offline_iters,)
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--seed', help='RNG seed', type=int, default=0)
+    parser.add_argument('--env', type=str, default='cartpole')
+    parser.add_argument('--num_episodes', type=int, default=100)
+    parser.add_argument('--horizon', type=int, default=500)
+    parser.add_argument('--iw_method', type=str, default='is')
+    parser.add_argument('--iw_norm', type=str, default='none')
+    parser.add_argument('--natural', type=bool, default=False)
+    parser.add_argument('--file_name', type=str, default='progress')
+    parser.add_argument('--bound', type=str, default='max-d2')
+    parser.add_argument('--delta', type=float, default=0.99)
+    parser.add_argument('--njobs', type=int, default=2)
+    parser.add_argument('--policy', type=str, default='nn')
+    parser.add_argument('--max_offline_iters', type=int, default=10)
+    parser.add_argument('--max_iters', type=int, default=500)
+    parser.add_argument('--gamma', type=float, default=1.0)
+    args = parser.parse_args()
+    assert args.njobs > 1, "Njobs must be more than 1, use POIS1 otherwise."
+    # Configure logging
+    if args.file_name == 'progress':
+        file_name = '%s_delta=%s_seed=%s_%s' % (args.env.upper(), args.delta, args.seed, time.time())
+    else:
+        file_name = args.file_name
+    logger.configure(dir='logs', format_strs=['stdout', 'csv'], file_name=file_name)
+    # Start training
+    train(env=args.env,
+          max_iters=args.max_iters,
+          num_episodes=args.num_episodes,
+          horizon=args.horizon,
+          iw_method=args.iw_method,
+          iw_norm=args.iw_norm,
+          natural=args.natural,
+          bound=args.bound,
+          delta=args.delta,
+          gamma=args.gamma,
+          seed=args.seed,
+          policy=args.policy,
+          max_offline_iters=args.max_offline_iters,
+          njobs=args.njobs)
+
+
+if __name__ == '__main__':
+    main()
