f1f326d2d15c8319e200e2d4e9f13d8504c20a29
nicomon24 <nico.montali24@gmail.com>
2018-10-24 16:32:35 +0200

    Updated notebook, new experiment

--- notebooks/sacred_notebook_utils.py
+++ notebooks/sacred_notebook_utils.py
@@ -27,12 +27,21 @@ def load_runs(base_directory):
             del runs[run_number]
     return runs
 
+def recursive_json_selector(obj, selector):
+    try:
+        if selector is not None:
+            for qk in selector.split('.'):
+                obj = obj[qk]
+        return obj
+    except:
+        return None
+
 '''
     This function returns a filtered dictionary containing all the runs that
     meet the requirements specified with the query parameter. Parameters at
     different levels of the run specification are given using the dot notation.
 '''
-def filter_runs(query, runs):
+def filter_runs(query, runs, avoid_missing=True):
     keys = list(runs.keys())
     for key, value in query.items():
         # Check if the still unfiltered runs have the specified parameter. If not, remove them
@@ -40,10 +49,11 @@ def filter_runs(query, runs):
         for run_key in keys:
             obj = runs[run_key]
             # Dot notation at any level
-            for qk in key.split('.'):
-                obj = obj[qk]
+            obj = recursive_json_selector(obj, key)
             # Check if it matches value
-            if obj == value:
+            if obj is None and not avoid_missing:
+                _keys.append(run_key)
+            elif obj is not None and obj == value:
                 _keys.append(run_key)
         keys = _keys
     # Now create a filtered object only with the selected runs
