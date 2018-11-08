f9e5b54b9fdf8d17235583e771626073f469317d
WolfLo <lorenzo.lupo93@gmail.com>
2018-11-08 18:00:56 +0100

    cleaning inherited code

--- baselines/poise/poise.py
+++ baselines/poise/poise.py
@@ -10,12 +10,15 @@ from collections import deque
 from baselines import logger
 from baselines.common.cg import cg
 
+
 @contextmanager
 def timed(msg):
     print(colorize(msg, color='magenta'))
     tstart = time.time()
     yield
-    print(colorize('done in %.3f seconds'%(time.time() - tstart), color='magenta'))
+    print(colorize(
+        'done in %.3f seconds' % (time.time() - tstart), color='magenta'))
+
 
 def traj_segment_generator(pi, env, n_episodes, horizon, stochastic=True):
     """
@@ -97,6 +100,7 @@ def traj_segment_generator(pi, env, n_episodes, horizon, stochastic=True):
         # Next step
         t += 1
 
+
 def add_disc_rew(seg, gamma):
     """
     Adds discounted rewards and returns to the dataset
@@ -128,7 +132,8 @@ def add_disc_rew(seg, gamma):
 def optimize_offline(max_offline_ite=100):
     for i in range(max_offline_ite):
 
-    return
+        return
+
 
 def render(env, pi, horizon):
     """
@@ -148,6 +153,7 @@ def render(env, pi, horizon):
         time.sleep(0.1)
         env.render()
         t += 1
+
 
 def learn(make_env, make_policy, *,
           horizon,
@@ -169,60 +175,67 @@ def learn(make_env, make_policy, *,
         max_iters: total number of learning iteration
     """
 
-    #Print options
+    # Print options
     np.set_printoptions(precision=3)
 
-    #Build the environment
+    # Build the environment
     env = make_env()
     ob_space = env.observation_space
     ac_space = env.action_space
     max_samples = horizon
 
-    #Build the policy
+    # Build the policy
     pi = make_policy('pi', ob_space, ac_space)
 
-    #Get all learnable parameters
+    # Get all learnable parameters
     all_var_list = pi.get_trainable_variables()
-    var_list = [v for v in all_var_list if v.name.split('/')[1].startswith('pol')]
+    var_list = [v for v in all_var_list
+                if v.name.split('/')[1].startswith('pol')]
     shapes = [U.intprod(var.get_shape().as_list()) for var in var_list]
     n_parameters = sum(shapes)
 
-    #Placeholders
+    # Placeholders
     ob_ = ob = U.get_placeholder_cached(name='ob')
     ac_ = pi.pdtype.sample_placeholder([max_samples], name='ac')
     mask_ = tf.placeholder(dtype=tf.float32, shape=(max_samples), name='mask')
     rew_ = tf.placeholder(dtype=tf.float32, shape=(max_samples), name='rew')
-    disc_rew_ = tf.placeholder(dtype=tf.float32, shape=(max_samples), name='disc_rew')
-    gradient_ = tf.placeholder(dtype=tf.float32, shape=(n_parameters, 1), name='gradient')
+    disc_rew_ = tf.placeholder(dtype=tf.float32, shape=(max_samples),
+                               name='disc_rew')
+    gradient_ = tf.placeholder(dtype=tf.float32,
+                               shape=(n_parameters, 1), name='gradient')
     iter_number_ = tf.placeholder(dtype=tf.int32, name='iter_number')
     losses_with_name = []
 
-    #Episodic return
+    # Episodic return
     ep_return = tf.reduce_sum(mask_ * disc_rew_)
 
-    #Other Tf definitions
+    # Other Tf definitions
     # ...
 
-    #Info
+    # Info
     losses_with_name.extend([(ep_return, 'Return')])
     losses, loss_names = map(list, zip(*losses_with_name))
-    compute_losses = U.function([ob_, ac_, rew_, disc_rew_, mask_, iter_number_], losses)
+    compute_losses = U.function(
+        [ob_, ac_, rew_, disc_rew_, mask_, iter_number_], losses)
 
-    #Tf utils
+    # Tf utils
     assert_ops = tf.group(*tf.get_collection('asserts'))
     print_ops = tf.group(*tf.get_collection('prints'))
     set_parameter = U.SetFromFlat(var_list)
     get_parameter = U.GetFlat(var_list)
 
-    #Set sampler (default: sequential)
+    # Set sampler (default: sequential)
     if sampler is None:
-        seg_gen = traj_segment_generator(pi, env, 1, horizon, stochastic=True) #collect ONE trajectory
-        sampler = type("SequentialSampler", (object,), {"collect": lambda self, _: seg_gen.__next__()})()
+        # Rb:collect only ONE trajectory
+        seg_gen = traj_segment_generator(pi, env, 1,
+                                         horizon, stochastic=True)
+        sampler = type("SequentialSampler", (object,),
+                       {"collect": lambda self, _: seg_gen.__next__()})()
 
-    #Tf initialization
+    # Tf initialization
     U.initialize()
 
-    #Learning loop
+    # Learning loop
     episodes_so_far = 0
     timesteps_so_far = 0
     iters_so_far = 0
@@ -231,63 +244,64 @@ def learn(make_env, make_policy, *,
     while True:
         iters_so_far += 1
 
-        #Render one episode
+        # Render one episode
         if render_after is not None and iters_so_far % render_after == 0:
             if hasattr(env, 'render'):
                 render(env, pi, horizon)
 
-        #Custom callback
+        # Custom callback
         if callback:
             callback(locals(), globals())
 
-        #Exit loop in the end
+        # Exit loop in the end
         if iters_so_far >= max_iters:
             print('Finished...')
             break
 
-        #Learning iteration
+        # Learning iteration
         logger.log('********** Iteration %i ************' % iters_so_far)
 
-        #Generate trajectories
+        # Generate trajectories
         theta = get_parameter()
         with timed('sampling'):
             seg = sampler.collect(theta)
 
-        #Retrieve data
+        # Retrieve data
         add_disc_rew(seg, gamma)
         lens, u_rets = seg['ep_lens'], seg['ep_rets']
-        assert len(lens)==1
+        assert len(lens) == 1
         episodes_so_far += 1
         timesteps_so_far += lens[0]
-        args = ob, ac, rew, disc_rew, mask, iter_number = seg['ob'], seg['ac'], seg['rew'], seg['disc_rew'], seg['mask'], iters_so_far
+        args = ob, ac, rew, disc_rew, mask, iter_number = \
+            seg['ob'], seg['ac'], seg['rew'], seg['disc_rew'], seg['mask'], iters_so_far
 
-        #Info
+        # Info
         with timed('summaries before'):
             logger.record_tabular("Iteration", iters_so_far)
             logger.record_tabular("URet", u_rets[0])
             logger.record_tabular("TimestepsSoFar", timesteps_so_far)
             logger.record_tabular("TimeElapsed", time.time() - tstart)
 
-        #Save policy parameters to disk
+        # Save policy parameters to disk
         if save_weights:
             logger.record_tabular('Weights', str(get_parameter()))
             import pickle
             file = open('checkpoint.pkl', 'wb')
             pickle.dump(theta, file)
 
-        #Perform optimization
+        # Perform optimization
         with timed("Optimization"):
             pass
-            #...
+            # ...
 
-        #Info
+        # Info
         with timed('summaries after'):
             meanlosses = np.array(compute_losses(*args))
             for (lossname, lossval) in zip(loss_names, meanlosses):
                 logger.record_tabular(lossname, lossval)
 
-        #Print all info in a table
+        # Print all info in a table
         logger.dump_tabular()
 
-    #Close environment in the end
+    # Close environment in the end
     env.close()
