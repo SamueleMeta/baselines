f4fd0e7ef700dd361302a867fd64ca440d65f977
WolfLo <lorenzo.lupo93@gmail.com>
2019-01-12 18:48:33 +0100

    ucb1 vs pbpoise first anaysis

--- baselines/pbpoise/run.py
+++ baselines/pbpoise/run.py
@@ -179,7 +179,7 @@ def multiple_runs(args):
 
     seed = []
     # for i in [n/10 for n in range(1, 8)]:
-    for k in range(0, 31):
+    for k in range(0, 30):
         seed.append(k)
 
     # Parallelize single runs
