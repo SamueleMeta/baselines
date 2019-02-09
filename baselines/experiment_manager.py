26ae64dd9a383f3320c3fa54bb093ede8fe13b42
nicomon24 <nico.montali24@gmail.com>
2019-02-09 15:18:50 +0100

    Completed v1 of experiment view

--- baselines/experiment_manager.py
+++ baselines/experiment_manager.py
@@ -118,7 +118,7 @@ elif args.command == 'view':
         run = running_runs[key]
         print("Run: {0} ({1})".format(key, run['config']['env']))
         print("\tSteps: {0}/{1}\t\tReward: {2}".format(len(run['metrics']['EpRewMean']['steps']), run['config']['max_iters'], run['metrics']['EpRewMean']['values'][-1]))
-        steps = len(run['metrics']['EpRewMean']['steps']
+        steps = len(run['metrics']['EpRewMean']['steps'])
         start_time = run['run']['start_time']
         print(start_time, type(start_time))
 
