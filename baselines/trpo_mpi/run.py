fa9f5e368168acc305a9989247c0ff00c5aba6f4
nicomon24 <nico.montali24@gmail.com>
2019-02-14 18:00:22 +0100

    Added max iters

--- baselines/trpo_mpi/run.py
+++ baselines/trpo_mpi/run.py
@@ -109,7 +109,7 @@ def train(env, policy, policy_init, n_episodes, horizon, seed, njobs=1, save_wei
     env = make_env()
     trpo_mpi.learn(env, make_policy, batch_size=n_episodes, task_horizon=horizon,
                 max_kl=alg_args['max_kl'], cg_iters=alg_args['cg_iters'],
-                gamma=alg_args['gamma'], lam=alg_args['lam'])
+                gamma=alg_args['gamma'], lam=alg_args['lam'], max_iters=alg_args['max_iters'])
 
 def main():
     import argparse
