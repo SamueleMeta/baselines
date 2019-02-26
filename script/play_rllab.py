7e42df2f662562b9d1c78e442157dacadade8f89
nicomon24 <nico.montali24@gmail.com>
2019-02-26 14:19:41 +0100

    Removed clip from MLP policy

--- script/play_rllab.py
+++ script/play_rllab.py
@@ -36,27 +36,27 @@ from baselines.common.parallel_sampler import ParallelSampler
 # ============================
 # HARD CODED WEIGHTS
 # ============================
-WEIGHTS = list(map(lambda x: x.rstrip(' \r\n') if len(x.rstrip(' \r\n')) > 0 else None, """0.15953386  0.10822787  0.30392429 -0.19635692 -0.47422308  0.14848398
- -0.19973465 -0.36574065  0.40281759 -0.08021425 -0.01819268 -0.06295348
-  0.22438566 -0.02605306  0.30656943  0.61153343  0.21480066 -0.51247014
- -0.23002151 -0.30995863  0.2287879   0.08066655 -0.32371414  0.56897408
- -0.18204293 -0.50848639  0.43094811  0.70952242  0.38075873  0.4604741
-  1.02463092  0.17539713  0.81328516 -0.35254996  0.07754536  0.77557677
-  0.34065107 -0.48293102  0.04958217 -0.14484813  0.45851386  0.1602454
-  0.41681276  0.30192835  0.58199219  0.73821455 -0.44703031 -0.17168907
- -0.30993005 -0.36565573  0.7119807   0.1930647   0.37343778  0.83623938
- -0.61747588  0.52922941  0.16388354  0.03601739 -0.34726328  0.00309361
- -0.41042698 -0.31262208 -0.17347692 -0.2047279  -0.10537838 -0.08110084
- -0.03735622  0.03708094 -0.02780432 -0.06436927 -0.10838084  0.10670034
- -1.28798893  0.49476737  0.04617231 -0.41995246  0.09265398 -0.02882002
-  0.50526057  0.30239238 -0.50592869 -0.42284231  0.36936024  0.57067307
-  0.10089432  0.46859915  0.1611406  -0.62135622 -1.51016674  1.11468115
-  0.09764898 -1.17192074  0.2054546  -0.23601387  0.69082661  0.20433299
- -0.32798772""".replace('\n',' ').rstrip(' \r\n').split(' ')))
+WEIGHTS = list(map(lambda x: x.rstrip(' \r\n') if len(x.rstrip(' \r\n')) > 0 else None, """0.05305602 -0.18459691  0.70950599  0.53141615  0.31627853 -0.29742844
+ -0.61745293  0.51616656 -0.31569655 -0.33130817  0.40292603 -0.29140672
+ -0.57827868 -0.13929554  0.21483592 -0.15595037  0.17454522 -0.19326297
+  0.25094717  0.83368002  0.22152866 -0.18484083  0.03383261  0.45452386
+ -0.38192054 -0.33433183 -0.03796898 -0.49614562  0.26963035  0.63315435
+  0.30569661  0.54124749  0.49595556 -0.22250939 -1.10102485 -0.49956239
+ -0.46548248  0.20697958 -1.34049531 -0.30234341  0.26796947  0.23690754
+ -0.69478062  1.7125646  -0.67662763  1.93259401  0.21739732  1.49646857
+ -0.20361122  0.04088948 -0.21100797 -0.47424338  0.40109244  0.10899712
+ -0.35579484  0.13663568  0.08160883  0.29034328  0.6590512  -0.54917494
+  0.31953252  1.38984592 -0.09815401  0.05032331 -0.00637668  0.50819017
+  0.07671488  0.52851077 -0.17194649 -0.15104466  0.08020897  0.33621285
+  0.18527502 -0.19619583 -0.02143036  0.06078397 -0.21261653 -0.01708933
+  0.56292861 -0.15359024 -0.43026468  1.67854235 -1.50268328  0.75034172
+ -0.5788268   0.26711257 -1.70169838  0.36958352  0.46216935  0.01091633
+ -0.83637988 -1.50260334 -0.62852089 -1.48288064 -1.77355002  1.12855729
+ -2.63364337""".replace('\n',' ').rstrip(' \r\n').split(' ')))
 WEIGHTS = [w for w in WEIGHTS if w is not None]
 WEIGHTS = list(map(float, WEIGHTS))
-# ============================
 
+# ============================
 
 def play_episode(env, pi, gamma, horizon=500):
     ob = env.reset()
@@ -65,9 +65,14 @@ def play_episode(env, pi, gamma, horizon=500):
     disc = 1.0
     timesteps = 0
     frames = []
-    while not done and timesteps < horizon:
-        a, _ = pi.get_action(ob)
-        ob, r, done, _ = env.step(0)
+    while not done and (horizon < 0 or timesteps < horizon):
+        if isinstance(pi, GaussianMLPPolicy):
+            a, _ = pi.get_action(ob)
+        elif isinstance(pi, MlpPolicy):
+            a, vpred = pi.act(True, ob)
+        else:
+            raise Exception('Unrecognized policy')
+        ob, r, done, _ = env.step(a)
         reward += r
         disc_reward += r * disc
         disc *= gamma
@@ -78,15 +83,44 @@ def play_episode(env, pi, gamma, horizon=500):
         'frames': frames
     }
 
-def create_policy_and_env(env, seed, policy, weights):
-    # Session
-    sess = U.single_threaded_session()
-    sess.__enter__()
+def create_env(env, seed):
+    if env.startswith('rllab.'):
+        # Get env name and class
+        env_name = re.match('rllab.(\S+)', env).group(1)
+        env_rllab_class = rllab_env_from_name(env_name)
+        # Define env maker
+        def make_env():
+            env_rllab = env_rllab_class()
+            _env = Rllab2GymWrapper(env_rllab)
+            return _env
+        # Used later
+        env_type = 'rllab'
+    else:
+        # Normal gym, get if Atari or not.
+        env_type = get_env_type(env)
+        assert env_type is not None, "Env not recognized."
+        # Define the correct env maker
+        if env_type == 'atari':
+            # Atari, custom env creation
+            def make_env():
+                _env = make_atari(env)
+                return wrap_deepmind(_env)
+        else:
+            # Not atari, standard env creation
+            def make_env():
+                env_rllab = gym.make(env)
+                return env_rllab
+    env = make_env()
+    env.seed(seed)
+    return env
 
-    # Create env
-    env_class = rllab_env_from_name(env)
-    env = normalize(env_class())
+def create_env_rllab(env, seed):
+    env_name = re.match('rllab.(\S+)', env).group(1)
+    env_rllab_class = rllab_env_from_name(env_name)
+    env = normalize(env_rllab_class())
+    return env
 
+def create_policy_rllab(policy, env, weights):
     # Create policy
     obs_dim = env.observation_space.flat_dim
     action_dim = env.action_space.flat_dim
@@ -118,10 +152,44 @@ def create_policy_and_env(env, seed, policy, weights):
     else:
         weights = WEIGHTS
     policy.set_param_values(weights)
+    return policy
 
-    print(policy.get_param_values())
+def create_policy_baselines(policy, env, weights):
+    ob_space = env.observation_space
+    ac_space = env.action_space
+    # Make policy
+    if policy == 'linear':
+        hid_size = num_hid_layers = 0
+    elif policy == 'simple-nn':
+        hid_size = [16]
+        num_hid_layers = 1
+    elif policy == 'nn':
+        hid_size = [100, 50, 25]
+        num_hid_layers = 3
 
-    return env, policy
+    policy_initializer = U.normc_initializer(0.0)
+    if policy == 'linear' or policy == 'nn' or policy == 'simple-nn':
+        def make_policy(name, ob_space, ac_space):
+            return MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
+                             hid_size=hid_size, num_hid_layers=num_hid_layers, gaussian_fixed_var=True, use_bias=True, use_critic=False,
+                             hidden_W_init=policy_initializer,
+                             output_W_init=policy_initializer)
+    elif policy == 'cnn':
+        def make_policy(name, ob_space, ac_space):
+            return CnnPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
+                         gaussian_fixed_var=True, use_bias=False, use_critic=False,
+                         hidden_W_init=policy_initializer,
+                         output_W_init=policy_initializer)
+    else:
+        raise Exception('Unrecognized policy type.')
+    pi = make_policy('pi', ob_space, ac_space)
+    # Set the weights
+    if weights is not None:
+        raise Exception('TODO load pickle file.')
+    else:
+        weights = WEIGHTS
+    pi.set_param(weights)
+    return pi
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -135,8 +203,19 @@ def main():
     parser.add_argument('--command', type=str, choices=['stats', 'render'])
     args = parser.parse_args()
 
-    env, pi = create_policy_and_env(args.env, args.seed, args.policy, args.weights)
+    # Session
+    sess = U.single_threaded_session()
+    sess.__enter__()
 
+    METHOD = 'baselines'
+    if METHOD == 'rllab':
+        env = create_env(args.env, args.seed)
+        pi = create_policy_rllab(args.policy, env.rllab_env, args.weights)
+    else:
+        env = create_env(args.env, args.seed)
+        pi = create_policy_baselines(args.policy, env, args.weights)
+
+    # Do rollouts
     N = 1 if args.command == 'render' else args.episodes
     rewards = []
     for i in trange(N):
