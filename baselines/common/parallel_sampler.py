10ddb1ee7d108bbf0e6bf02600052e0fc29343c1
albertometelli <alberto.metelli@gmail.com>
2019-09-16 20:35:46 +0200

    .

--- baselines/common/parallel_sampler.py
+++ baselines/common/parallel_sampler.py
@@ -226,7 +226,7 @@ class ParallelSampler(object):
 
         new_field = np.zeros(shape_tuple)
 
-        for i, start, stop in enumerate(zip(starts, stops)):
+        for i, (start, stop) in enumerate(zip(starts, stops)):
             new_field[i*horizon_eff:ep_lens[i]] = field[start:stop]
             if pad_value is None:
                 new_field[ep_lens[i]:(i + 1)*horizon_eff] = field[stop-1]
