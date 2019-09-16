e8d8cdc7f853c6079a10f97c6f02313ed645f507
albertometelli <alberto.metelli@gmail.com>
2019-09-16 20:57:26 +0200

    .

--- baselines/pois/pois.py
+++ baselines/pois/pois.py
@@ -276,8 +276,8 @@ def learn(make_env, make_policy, *,
         rew_split = rew_split - (tf.reduce_sum(rew_split) / (tf.reduce_sum(mask_split) + 1e-24))
 
     #discounter = [pow(gamma, i) for i in range(0, horizon)] # Decreasing gamma
-    print(horizon_eff_)
-    discounter_tf = gamma ** tf.range(horizon_eff_) #tf.constant(discounter)
+    #print(horizon_eff_)
+    discounter_tf = gamma ** tf.range(horizon_eff_, dtype=tf.float32) #tf.constant(discounter)
     disc_rew_split = rew_split * discounter_tf
 
     #tf.add_to_collection('prints', tf.Print(ep_return, [ep_return], 'ep_return_not_clustered', summarize=20))
