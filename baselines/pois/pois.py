b52d47d6f0d3559185c4bf52d6ddd336b9e0c16f
albertometelli <alberto.metelli@gmail.com>
2019-09-16 20:40:23 +0200

    .

--- baselines/pois/pois.py
+++ baselines/pois/pois.py
@@ -605,7 +605,7 @@ def learn(make_env, make_policy, *,
         # Get clustered reward
         ob, ac, rew, disc_rew, mask = seg['ob'], seg['ac'], seg['rew'], seg['disc_rew'], seg['mask']
 
-        reward_matrix = np.reshape(disc_rew * mask, (n_episodes_eff_, -1))
+        reward_matrix = np.reshape(disc_rew * mask, tf.concat([n_episodes_eff_, tf.constant(-1)]))
         ep_reward = np.sum(reward_matrix, axis=1)
         ep_reward = cluster_rewards(ep_reward, reward_clustering)
 
