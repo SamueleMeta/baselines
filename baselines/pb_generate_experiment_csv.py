5799e5550ac3d850cbbecb7d6d97fa28e4ab794a
Matteo <matteo.papini@polimi.it>
2019-12-06 14:14:55 +0100

    updated pbpomis experiment

--- baselines/pb_generate_experiment_csv.py
+++ baselines/pb_generate_experiment_csv.py
@@ -1,14 +1,14 @@
 script = ['pbpomis/run']
 
-env = ['rllab.cartpole',
-        'rllab.mountain-car',
-        'rllab.inverted-pendulum',
-        'rllab.acrobot',
-        'rllab.inverted-double-pendulum']
+env = ['rllab.cartpole']#,
+        #'rllab.mountain-car',
+        #'rllab.inverted-pendulum',
+        #'rllab.acrobot',
+        #'rllab.inverted-double-pendulum']
 
 policy = ['linear']
 
-delta = [0.2, 0.4, 0.6, 0.8, 0.9]
+delta = [0.05, 0.1, 0.2, 0.3, 0.4]
 
 gamma = [1.]
 
@@ -16,13 +16,13 @@ njobs = [1]
 
 seed = [10, 109, 904, 160, 570]
 
-capacity = [1, 5, 10]
+capacity = [1, 10]
 
 num_episodes = [10]
 
 max_iters = [500]
 
-iw_norm = ['rows', 'all']
+iw_norm = ['rows']
 
 vars = dict(filter(lambda k: not k[0].startswith('__'), globals().items()))
 
