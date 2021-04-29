Add options for experiments w/ and w/o penalization and fixed variance

baselines/optimalMis/opt_pomis.py
@@ -202,6 +202,9 @@ def learn(make_env, make_policy, *,
           reward_clustering='none',
           capacity=10,
           inner=10,
+          penalization=True,
+          learnable_variance=True,
+          variance_initializer=-1,
           warm_start=True):
 
     np.set_printoptions(precision=3)
@@ -357,7 +360,10 @@ def learn(make_env, make_policy, *,
     if bound == 'J':
         bound_ = w_return_mean
     elif bound == 'max-d2-harmonic':
-        bound_ = - w_return_mean - tf.sqrt((1 - delta) / (delta * ess_divergence_harmonic)) * return_abs_max**2
+        if penalization:
+            bound_ = - w_return_mean - tf.sqrt((1 - delta) / (delta * ess_divergence_harmonic)) * return_abs_max**2
+        else:
+            bound_ = - w_return_mean
         lower_bound = - w_return_mean_lb + tf.sqrt((1 - delta) / (delta * ess_renyi_harmonic)) * return_abs_max**2
     elif bound == 'max-d2-arithmetic':
         bound_ = - w_return_mean - tf.sqrt(1 / (delta * ess_renyi_arithmetic)) * return_abs_max**2
@@ -537,6 +543,9 @@ def learn(make_env, make_policy, *,
                 logger.record_tabular("TimestepsSoFar", timesteps_so_far)
                 logger.record_tabular("TimeElapsed", time.time() - tstart)
                 logger.record_tabular("WReturnMean", compute_w_return(*args)[0])
+                logger.record_tabular("Penalization", penalization)
+                logger.record_tabular("LearnableVariance", learnable_variance)
+                logger.record_tabular("VarianceInitializer", variance_initializer)
 
             if save_weights > 0 and iters_so_far % save_weights == 0:
                 logger.record_tabular('Weights', str(get_parameter()))

baselines/policy/mlp_policy.py
@@ -20,7 +20,8 @@ class MlpPolicy(object):
 
     def _init(self, ob_space, ac_space, hid_size, num_hid_layers, gaussian_fixed_var=True, use_bias=True, use_critic=True,
               seed=None, hidden_W_init=U.normc_initializer(1.0), hidden_b_init=tf.zeros_initializer(),
-                 output_W_init=U.normc_initializer(0.01), output_b_init=tf.zeros_initializer()):
+                 output_W_init=U.normc_initializer(0.01), output_b_init=tf.zeros_initializer(),
+                    learnable_variance = True, variance_initializer = -1):
         """Params:
             ob_space: task observation space
             ac_space : task action space
@@ -69,7 +70,12 @@ class MlpPolicy(object):
                                        name='final',
                                        kernel_initializer=output_W_init,
                                        use_bias=use_bias)
-                self.logstd = logstd = tf.get_variable(name="pol_logstd", shape=[1, pdtype.param_shape()[0]//2], initializer=output_b_init)
+                if learnable_variance:
+                    self.logstd = logstd = tf.get_variable(name="pol_logstd", shape=[1, pdtype.param_shape()[0]//2], initializer=output_b_init)
+                else:
+                    self.logstd = logstd = tf.get_variable(name="pol_logstd", shape=[1, pdtype.param_shape()[0] // 2],
+                                                           trainable=False, initializer=tf.constant_initializer(value=variance_initializer))
+
                 pdparam = tf.concat([mean, mean * 0.0 + logstd], axis=1)
             else:
                 pdparam = tf.layers.dense(last_out, pdtype.param_shape()[0],